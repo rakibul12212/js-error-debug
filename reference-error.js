a31a1368206699b477c646c1f572d95fb7d072d0
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-03-13 01:17:29 +0600

    f

--- reference-error.js
+++ reference-error.js
@@ -3,6 +3,15 @@ const y = 'moni';
 //console.log(z); error
 
 
-console.log(name);
+//console.log(name); error
 //const name = "rakib";
 let name = "rakib";
+
+
+//console.log(add(2, 3));
+//console.log(add2(2, 3)); error
+
+function add(a, b) {
+    return a + b
+}
+//const add2 = (a, b) => a + b; errror
